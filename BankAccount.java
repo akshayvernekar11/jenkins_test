f244c21d843e7c6362ff676dda3f04d757b4f3f5
Akshay <akshayvernekar11@gmail.com>
2018-09-02 21:25:22 +0530

    commit

---
+++ BankAccount.java
@@ -0,0 +1,20 @@
+package Jenkin.CI.CD.test;
+
+public class BankAccount {
+
+  private double balance;
+
+  public BankAccount(double balance) {
+    this.balance = balance;
+  }
+
+  public double debit(double amount) {
+    if (balance < amount) {
+      amount = balance;
+    }
+
+    balance -= amount;
+    return amount;
+  }
+
+}
